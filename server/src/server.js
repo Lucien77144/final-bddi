7f6a0e1479d4ef077390010d1213224edfd036ed
Antoine Droyer <antoine.droyer@gmail.com>
2023-03-06 16:33:22 +0100

    oui

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,18 @@
-var express = require('express'),
-    app = express(),
-    http = require('http'),
-    socketIO = require('socket.io'),
-    server, io;
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
-app.get('/', function (req, res) {
-res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname));
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
 });
 
-server = http.Server(app);
-server.listen(3000);
-
-io = socketIO(server);
-
-io.on('connection', function (socket) {
-  socket.emit('greeting-from-server', {
-      greeting: 'Hello Client'
-  });
-  socket.on('greeting-from-client', function (message) {
-    console.log(message);
-  });
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+  console.log(__dirname);
 });

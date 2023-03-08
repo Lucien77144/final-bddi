92c29aa5ebe6d87047159c91626f09dc38763fae
Antoine Droyer <antoine.droyer@gmail.com>
2023-03-08 14:49:16 +0100

    in progress -> client connexion

--- server/src/server.js
+++ server/src/server.js
@@ -1,65 +1,24 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  // Établir une nouvelle connexion Socket.io
+  const socket = io();
+
+  socket.emit('message', 'Connexion établie !');
+  // Envoyer une réponse à la requête GET
+  res.send('Requête GET reçue');
 });
-
-const MAX_USERS_PER_ROOM = 2;
-const rooms = {};
-
-// Vérifier si une room existe
-function findAvailableRoom() {
-  for (const roomId in rooms) {
-    if (rooms[roomId].length < MAX_USERS_PER_ROOM) {
-      return roomId;
-    }
-  }
-  return null;
-}
-
-// Créer une nouvelle room avec un identifiant unique
-function createRoom() {
-  const roomId = Math.random().toString(36).substring(7);
-  rooms[roomId] = [];
-  return roomId;
-}
 
 io.on('connection', (socket) => {
-  console.log('Nouvelle connexion :', socket.id);
+  console.log('Nouvelle connexion établie');
 
-  // Vérifier si une room est disponible
-  const roomId = findAvailableRoom();
-  if (roomId) {
-    console.log('Utilisateur rejoint la room existante :', roomId);
-    socket.join(roomId);
-    rooms[roomId].push(socket.id);
-  } else {
-    // Créer une nouvelle room et rediriger l'utilisateur vers cette room
-    console.log('Création d\'une nouvelle room');
-    const newRoomId = createRoom();
-    socket.join(newRoomId);
-    rooms[newRoomId].push(socket.id);
-    socket.emit('redirect', '/addRoom');
-  }
-
-  socket.on('disconnect', () => {
-    console.log('Déconnexion :', socket.id);
-    // Retirer l'utilisateur de la room correspondante
-    for (const roomId in rooms) {
-      const index = rooms[roomId].indexOf(socket.id);
-      if (index !== -1) {
-        rooms[roomId].splice(index, 1);
-        break;
-      }
-    }
-  });
+  // Envoyer un message à la nouvelle connexion
+  socket.emit('message', 'Connexion établie !');
 });
 
-server.listen(3000, () => {
-  console.log('Serveur démarré sur le port 3000');
+http.listen(3000, () => {
+  console.log('Le serveur écoute sur le port 3000');
 });

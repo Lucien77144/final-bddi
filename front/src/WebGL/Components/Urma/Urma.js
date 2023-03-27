b907918b8865107b77b2edd7cfe56e043c24c436
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2023-03-27 18:38:17 +0200

    feat(urma): deplacements:

--- front/src/WebGL/Components/Urma/Urma.js
+++ front/src/WebGL/Components/Urma/Urma.js
@@ -4,6 +4,21 @@ import vertexShader from "./shaders/vertexShader.glsl";
 import { BoxGeometry, Mesh, ShaderMaterial, Vector3 } from "three";
 import InputManager from "utils/InputManager.js";
 
+let isMoving = {
+  left: false,
+  right: false,
+};
+let isEnding = {
+  left: false,
+  right: false,
+};
+let startTime = 0;
+let endTime = 0;
+let velocity = 0;
+let endVelocity = 0;
+
+const speedDelay = 250;
+
 export default class Urma {
   constructor(_position = new Vector3(0, 0, 0)) {
     this.experience = new Experience();
@@ -37,37 +52,64 @@ export default class Urma {
   }
 
   setInputs() {
-    const getDeltaTime = (value) => {
-
-    }
-    let isMoving = false;
-    let startTime;
-    let velocity = 0;
     InputManager.on("right", (value) => {
-      if (value && !isMoving) {
-        isMoving = true;
-      } else if (!value && isMoving) {
-        startTime = null;
-        isMoving = false;
-      } else if(isMoving && value){
-        startTime = startTime ? this.time.current - startTime : this.time.current;
-        console.log(startTime);
-      };
+      if (value && !isMoving.right) {
+        isMoving.right = true;
+        startTime = this.time.current;
+        endTime = 0;
+        endVelocity = 0;
+        velocity = 0;
+        isEnding.right = false;
+        isEnding.left = false;
+      } else if (!value && isMoving.right) {
+        isEnding.right = true;
+        endTime = this.time.current;
+        setTimeout(() => {
+          isEnding.right = false;
+          isMoving.right = false;
+        }, speedDelay);
+      }
     });
     InputManager.on("left", (value) => {
-      if (value && !isMoving) {
-        isMoving = true;
-      } else if (!value && isMoving) {
-        isMoving = false;
+      if (value && !isMoving.left) {
+        isMoving.left = true;
+        startTime = this.time.current;
+        endTime = 0;
+        endVelocity = 0;
+        velocity = 0;
+        isEnding.right = false;
+        isEnding.left = false;
+      } else if (!value && isMoving.left) {
+        isEnding.left = true;
+        endTime = this.time.current;
+        setTimeout(() => {
+          isEnding.left = false;
+          isMoving.left = false;
+        }, speedDelay);
       }
     });
   }
 
   update() {
     this.updatePosition();
+    if(isMoving.left || isMoving.right) {
+      velocity = (this.time.current - startTime) / speedDelay;
+      velocity = velocity > 1 ? 1 : velocity;
+
+      endVelocity = (this.time.current - endTime) / speedDelay;
+      endVelocity = endVelocity > 1 ? 1 : endVelocity;
+
+      velocity -= (isEnding.left || isEnding.right) ? endVelocity : 0;
+      this.updatePosition();
+    } else {
+      startTime = 0;
+      endTime = 0;
+      endVelocity = 0;
+    }
   }
 
   updatePosition() {
-    
+    this.mesh.position.z = isMoving.left ? this.mesh.position.z + (velocity / 10) : this.mesh.position.z;
+    this.mesh.position.z = isMoving.right ? this.mesh.position.z - (velocity / 10) : this.mesh.position.z;
   }
 }

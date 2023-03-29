0307e2b63ef91e6a4a5e0e74ef2b7b161efa1ccd
Amandine <quellec.amandine@gmail.com>
2023-03-29 18:12:04 +0200

    feat(movement): add fairy to grass scene

--- front/src/WebGL/Scenes/GrassScene.js
+++ front/src/WebGL/Scenes/GrassScene.js
@@ -1,9 +1,10 @@
 import Experience from "webgl/Experience.js";
 import Environment from "components/Environment.js";
 import { Vector3 } from "three";
-import GrassFloor from "../Components/Grass/GrassFloor.js";
-import Urma from "../Components/Urma/Urma.js";
-import Column from "../Components/Column/Column.js";
+import GrassFloor from "components/Grass/GrassFloor.js";
+import Urma from "components/Urma/Urma.js";
+import Column from "components/Column/Column.js";
+import FairyDust from "components/Fairy/FairyDust.js";
 
 export default class GrassScene {
   constructor() {
@@ -25,6 +26,7 @@ export default class GrassScene {
     // Setup
     this.environment = new Environment();
     this.floor = new GrassFloor();
+    this.fairyDust = new FairyDust();
     // this.rock1 = new Rock(new Vector3(0, 3, 0))
     // this.rock2 = new Rock(new Vector3(3, 3, 3))
     // this.rock3 = new Rock(new Vector3(6, 3, 6))
@@ -33,7 +35,8 @@ export default class GrassScene {
   }
 
   update() {
-    if(this.floor) this.floor.update();
-    if(this.urma) this.urma.update();
+    if (this.fairyDust) this.fairyDust.update();
+    if (this.floor) this.floor.update();
+    if (this.urma) this.urma.update();
   }
 }

eb54aaeb7088ffd3972b0494c918325186418e55
Antoine Droyer <antoine.droyer@gmail.com>
2023-04-28 11:21:21 +0200

    feat(fireflies) : add fireflies

--- front/src/WebGL/Scenes/config.js
+++ front/src/WebGL/Scenes/config.js
@@ -4,7 +4,7 @@ import Main from "scenes/Main.js";
 import CubeScene from "scenes/CubeScene";
 
 export default {
-  main: Main,
+  main: GrassScene,
   "cube": CubeScene,
   "fairy": FairyScene,
   "grass": GrassScene,

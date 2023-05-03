afd0dd14d55c3b2230f8cd1883e238ae25e86a03
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2023-05-03 16:07:53 +0200

    fix(wind): optimisation

--- front/src/WebGL/Components/newGrass/shaders/Grass/fragmentShader.glsl
+++ front/src/WebGL/Components/newGrass/shaders/Grass/fragmentShader.glsl
@@ -1,12 +1,7 @@
-uniform float uTime;
 uniform float uMaxBladeSize;
 uniform vec3 uBaseColor;
 
 varying vec3 vBasePosition;
-
-float rdm() {
-  return fract(sin(dot(vec3(uTime, 0.0, 0.0), vec3(12.9898, 78.233, 98.422)) * 43758.5453));
-}
 
 void main() {
   gl_FragColor = vec4(mix(uBaseColor, vec3(.04, .56, .27), vBasePosition.y / uMaxBladeSize), 1.);

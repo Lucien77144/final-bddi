52dbb280ceab61bd3c307dc8b448b4b02f353575
Antoine Droyer <antoine.droyer@gmail.com>
2023-05-09 12:08:20 +0200

    fix(backgrounds)

--- front/src/WebGL/Experience.js
+++ front/src/WebGL/Experience.js
@@ -26,15 +26,15 @@ export default class Experience {
     this.canvas = _canvas;
 
     // Set Ressources
-    this.debug = new Debug();
-    this.resources = new Resources(sources);
   }
-
+  
   setUp() {
     // Setup
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new Scene();
+    this.debug = new Debug();
+    this.resources = new Resources(sources);
     this.camera = new Camera();
     this.renderer = new Renderer(this.scene, this.camera);
     this.activeScene = new SceneManager();

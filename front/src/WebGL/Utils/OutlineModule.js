f47bdd9027304f9dd8f5025b6df3e4a83d33c15b
Amandine <quellec.amandine@gmail.com>
2023-05-31 16:20:44 +0200

    feat(debug-vignette): add dev

--- front/src/WebGL/Utils/OutlineModule.js
+++ front/src/WebGL/Utils/OutlineModule.js
@@ -19,10 +19,10 @@ export default class OutlineModule {
     this.grassScene = this.experience.activeScene;
     this.resources = this.experience.resources;
 
-        this.originalPosition = null;
-        this.originalUp = null;
-        this.onGame = false;
-        this.onLetter = false;
+    this.originalPosition = null;
+    this.originalUp = null;
+    this.onGame = false;
+    this.onLetter = false;
     this.debug = this.experience.debug;
 
     // Debug
@@ -49,9 +49,9 @@ export default class OutlineModule {
 
     this.mouseDown = false;
 
-        window.addEventListener('mousedown', (event) => {
-            this.mouseDown = true;
-        });
+    window.addEventListener("mousedown", (event) => {
+      this.mouseDown = true;
+    });
 
     window.addEventListener("mouseup", (event) => {
       this.mouseDown = false;
@@ -59,153 +59,158 @@ export default class OutlineModule {
       this.getInteractiveObjects(); // refresh interactive objects
     });
 
-        window.addEventListener('click', (event) => {
-            if (this.outlinePass.selectedObjects[0]?.interactive === true) {
-                this.activeObject = this.outlinePass.selectedObjects[0];
-                if(this.activeObject.name === 'controlPanelBase') {
-                    this.controlPanelChildren = this.activeObject.parent.children;
-                    this.controlPanelChildren.forEach((child) => {
-                        if (child.name.includes('Disk')) {
-                            child.interactive = true;
-                        }});
-                    if (this.activeObject.base) {
-                        this.base = this.activeObject;
-                        this.handleBaseClick();
-                    } else if (this.activeObject.disk) {
-                        this.handleDiskClick();
-                    }
-                } else if (this.activeObject.type === 'letter') {
-                    this.handleLetterClick();
-                }
+    window.addEventListener("click", (event) => {
+      if (this.outlinePass.selectedObjects[0]?.interactive === true) {
+        this.activeObject = this.outlinePass.selectedObjects[0];
+        if (this.activeObject.name === "controlPanelBase") {
+          this.controlPanelChildren = this.activeObject.parent.children;
+          this.controlPanelChildren.forEach((child) => {
+            if (child.name.includes("Disk")) {
+              child.interactive = true;
             }
-        });
-
-        window.addEventListener('keydown', (event) => {
-            if (this.onGame) {
-                if ((event.code === 'Space') || (event.code === 'Escape')) {
-                    this.returnCamera();
-        
-                    // Reset outlined object
-                    this.outlinePass.selectedObjects = [];
-                    
-                    // Reset active object
-                    this.activeObject = null;
-        
-                    // If any objects have been modified during the interaction, reset them
-                    // For example, you might reset any objects that have been moved or changed color
-                    this.interactiveObjects.forEach((object) => {
-                        // Add code here to reset each object to its original state
-                        object.interactive = false;
-                    });
-                    this.base.interactive = true;
-                }
-            } else if(this.onLetter) {
-                if (event.code === 'Space') {
-                    this.returnLetter();
-                    // ... rest of your event listener ...
-                }
-            }
-        });
-               
-        // Wait for resources
-        if (this.resources.loaded == this.resources.toLoad) {
-            this.buildUtils();
-        } else {
-            this.resources.on("ready", () => {
-                this.buildUtils();
-            });
+          });
+          if (this.activeObject.base) {
+            this.base = this.activeObject;
+            this.handleBaseClick();
+          } else if (this.activeObject.disk) {
+            this.handleDiskClick();
+          }
+        } else if (this.activeObject.type === "letter") {
+          this.handleLetterClick();
         }
+      }
+    });
+
+    window.addEventListener("keydown", (event) => {
+      if (this.onGame) {
+        if (event.code === "Space" || event.code === "Escape") {
+          this.returnCamera();
+
+          // Reset outlined object
+          this.outlinePass.selectedObjects = [];
+
+          // Reset active object
+          this.activeObject = null;
+
+          // If any objects have been modified during the interaction, reset them
+          // For example, you might reset any objects that have been moved or changed color
+          this.interactiveObjects.forEach((object) => {
+            // Add code here to reset each object to its original state
+            object.interactive = false;
+          });
+          this.base.interactive = true;
+        }
+      } else if (this.onLetter) {
+        if (event.code === "Space") {
+          this.returnLetter();
+          // ... rest of your event listener ...
+        }
+      }
+    });
+
+    // Wait for resources
+    if (this.resources.loaded == this.resources.toLoad) {
+      this.buildUtils();
+    } else {
+      this.resources.on("ready", () => {
+        this.buildUtils();
+      });
     }
+  }
 
-    handleLetterClick() {
-        this.onLetter = true;
-        // Define how far in front of the camera the object should appear
-        const distanceInFrontOfCamera = 5;
-    
-        // Get a new position in front of the camera
-        const direction = new THREE.Vector3();
-        this.camera.getWorldDirection(direction);
-        const newPosition = new THREE.Vector3();
-        newPosition.copy(this.camera.position).add(direction.multiplyScalar(distanceInFrontOfCamera));
-    
-        // Define the new scale you want for the object
-        const newScale = new THREE.Vector3(1, 1, 1); // Scale up by 3
-        
-        const newRotation = new THREE.Vector3(0, Math.PI / 2, 0);
-        // Use GSAP to animate the letter's scale and position
-        gsap.to(this.activeObject.parent.scale, {
-            duration: 1, // duration of the animation in seconds
-            x: newScale.x,
-            y: newScale.y,
-            z: newScale.z,
-            ease: "power1.out", // easing function for the animation
-        });
-    
-        gsap.to(this.activeObject.parent.position, {
-            duration: 1, // duration of the animation in seconds
-            x: newPosition.x,
-            y: newPosition.y,
-            z: newPosition.z,
-            ease: "power1.out", // easing function for the animation
-        });
+  handleLetterClick() {
+    this.onLetter = true;
+    // Define how far in front of the camera the object should appear
+    const distanceInFrontOfCamera = 5;
 
-        gsap.to(this.activeObject.parent.rotation, {
-            duration: 1, // duration of the animation in seconds
-            x: newRotation.x,
-            y: newRotation.y,
-            z: newRotation.z,
-            ease: "power1.out", // easing function for the animation
-        });
+    // Get a new position in front of the camera
+    const direction = new THREE.Vector3();
+    this.camera.getWorldDirection(direction);
+    const newPosition = new THREE.Vector3();
+    newPosition
+      .copy(this.camera.position)
+      .add(direction.multiplyScalar(distanceInFrontOfCamera));
 
-        const dialogBox = document.getElementById('dialogBox');
-        gsap.to(dialogBox.style, {
+    // Define the new scale you want for the object
+    const newScale = new THREE.Vector3(1, 1, 1); // Scale up by 3
 
-            duration: 0.5,
-            width: '500px', // The final width of the dialog box
-            height: '300px', // The final height of the dialog box
-            opacity: '1',
-            ease: "power1.out", // easing function for the animation
-        });
-    }
-    
-    returnLetter() {
-        this.onLetter = false;
-    
-        // Hide activeObject
-        this.activeObject.parent.visible = false;
-    
-        // Get the SVG element
-        const letterIcon = document.querySelector('.letter-icon');
-    
-        // Make the SVG visible
-        letterIcon.style.display = 'block';
-        
-        // letterIcon.style.transform = 'scale(2.5)';
-        // gsap.to(letterIcon.style, {
-        //     duration: 1, // duration of the animation in seconds
-        //     left: '0px', // replace with original x position
-        //     top: '0px', // replace with original y position
-        //     scale: 1, // replace with original scale
-        //     ease: "power1.out" // easing function for the animation
-        // });
+    const newRotation = new THREE.Vector3(0, Math.PI / 2, 0);
+    // Use GSAP to animate the letter's scale and position
+    gsap.to(this.activeObject.parent.scale, {
+      duration: 1, // duration of the animation in seconds
+      x: newScale.x,
+      y: newScale.y,
+      z: newScale.z,
+      ease: "power1.out", // easing function for the animation
+    });
 
-        //gsap from to
-        gsap.fromTo(letterIcon.style, {
-            left: '-40px', // replace with original x position
-            top: '-25px', // replace with original y position
-            scale: 2, // replace with original scale
-        }, {
-            duration: 1, // duration of the animation in seconds
-            left: '0px', // replace with original x position
-            top: '0px', // replace with original y position
-            scale: 1, // replace with original scale
-            ease: "power1.out" // easing function for the animation
-        });
-    }
-    
+    gsap.to(this.activeObject.parent.position, {
+      duration: 1, // duration of the animation in seconds
+      x: newPosition.x,
+      y: newPosition.y,
+      z: newPosition.z,
+      ease: "power1.out", // easing function for the animation
+    });
 
-    moveCamera() {
-        this.grassScene.onGame = true;
+    gsap.to(this.activeObject.parent.rotation, {
+      duration: 1, // duration of the animation in seconds
+      x: newRotation.x,
+      y: newRotation.y,
+      z: newRotation.z,
+      ease: "power1.out", // easing function for the animation
+    });
+
+    const dialogBox = document.getElementById("dialogBox");
+    gsap.to(dialogBox.style, {
+      duration: 0.5,
+      width: "500px", // The final width of the dialog box
+      height: "300px", // The final height of the dialog box
+      opacity: "1",
+      ease: "power1.out", // easing function for the animation
+    });
+  }
+
+  returnLetter() {
+    this.onLetter = false;
+
+    // Hide activeObject
+    this.activeObject.parent.visible = false;
+
+    // Get the SVG element
+    const letterIcon = document.querySelector(".letter-icon");
+
+    // Make the SVG visible
+    letterIcon.style.display = "block";
+
+    // letterIcon.style.transform = 'scale(2.5)';
+    // gsap.to(letterIcon.style, {
+    //     duration: 1, // duration of the animation in seconds
+    //     left: '0px', // replace with original x position
+    //     top: '0px', // replace with original y position
+    //     scale: 1, // replace with original scale
+    //     ease: "power1.out" // easing function for the animation
+    // });
+
+    //gsap from to
+    gsap.fromTo(
+      letterIcon.style,
+      {
+        left: "-40px", // replace with original x position
+        top: "-25px", // replace with original y position
+        scale: 2, // replace with original scale
+      },
+      {
+        duration: 1, // duration of the animation in seconds
+        left: "0px", // replace with original x position
+        top: "0px", // replace with original y position
+        scale: 1, // replace with original scale
+        ease: "power1.out", // easing function for the animation
+      }
+    );
+  }
+
+  moveCamera() {
+    this.grassScene.onGame = true;
 
     const targetPosition = new THREE.Vector3();
 
@@ -228,24 +233,23 @@ export default class OutlineModule {
     this.originalTarget = new THREE.Vector3();
     this.originalTarget.copy(this.camera.position).add(direction);
 
-
-        // Use GSAP to animate the camera's movement
-        gsap.to(this.camera.position, {
-            duration: 1, // duration of the animation in seconds
-            x: newPosition.x,
-            y: newPosition.y,
-            z: newPosition.z,
-            onUpdate: () => {
-                // Ensure the camera's up vector is set to signify the y-axis as up
-                this.camera.up.set(newUp.x, newUp.y, newUp.z);
-                this.camera.lookAt(-5, 2.5, 9);
-            },
-            onComplete: () => {
-                this.onGame = this.grassScene.onGame;
-            },
-            ease: "power1.out", // easing function for the animation
-        });
-    }
+    // Use GSAP to animate the camera's movement
+    gsap.to(this.camera.position, {
+      duration: 1, // duration of the animation in seconds
+      x: newPosition.x,
+      y: newPosition.y,
+      z: newPosition.z,
+      onUpdate: () => {
+        // Ensure the camera's up vector is set to signify the y-axis as up
+        this.camera.up.set(newUp.x, newUp.y, newUp.z);
+        this.camera.lookAt(-5, 2.5, 9);
+      },
+      onComplete: () => {
+        this.onGame = this.grassScene.onGame;
+      },
+      ease: "power1.out", // easing function for the animation
+    });
+  }
 
   returnCamera() {
     if (this.originalPosition && this.originalUp) {
@@ -302,16 +306,16 @@ export default class OutlineModule {
     this.setDebug();
   }
 
-    setShaderPath() {
-        return new ShaderPass({
-            uniforms: {
-                tDiffuse: { value: null },
-                vignette: { value: 0.5 },
-            },
-            vertexShader,
-            fragmentShader,
-        });
-    }
+  setShaderPath() {
+    return new ShaderPass({
+      uniforms: {
+        tDiffuse: { value: null },
+        vignette: { value: 0.5 },
+      },
+      vertexShader,
+      fragmentShader,
+    });
+  }
 
   setDebug() {
     if (this.debug.active) {
@@ -353,29 +357,29 @@ export default class OutlineModule {
       }
     });
 
-        this.getInteractiveObjects(); // Refresh interactive objects
-        this.activeObject = null;
+    this.getInteractiveObjects(); // Refresh interactive objects
+    this.activeObject = null;
 
-        // After the letter is clicked, show the dialog box
-        const dialogBox = document.getElementById('dialogBox');
+    // After the letter is clicked, show the dialog box
+    const dialogBox = document.getElementById("dialogBox");
 
-        if(dialogBox) {
-            dialogBox.textContent = 'Your text here...';  // Set the text before starting the animation
+    if (dialogBox) {
+      dialogBox.textContent = "Your text here..."; // Set the text before starting the animation
 
-            // Create a GSAP timeline
-            var tl = gsap.timeline();
-            tl.from(dialogBox, {opacity: 0, duration: 0.1})  // First animate the opacity
-            .to(dialogBox, {
-                paddingLeft: '20px',  // Then animate the padding
-                paddingRight: '20px',  // Then animate the padding
-                duration: 0.5,
-                opacity: 1,
-                ease: "power1.out",
-            });
-        } else {
-            console.log("Dialog box element not found");
-        }      
+      // Create a GSAP timeline
+      var tl = gsap.timeline();
+      tl.from(dialogBox, { opacity: 0, duration: 0.1 }) // First animate the opacity
+        .to(dialogBox, {
+          paddingLeft: "20px", // Then animate the padding
+          paddingRight: "20px", // Then animate the padding
+          duration: 0.5,
+          opacity: 1,
+          ease: "power1.out",
+        });
+    } else {
+      console.log("Dialog box element not found");
     }
+  }
 
   handleDiskClick() {
     // You might have additional behavior you want to implement when a disk is clicked.
@@ -393,32 +397,46 @@ export default class OutlineModule {
     this.getInteractiveObjects(); // Refresh interactive objects
   }
 
-    update() {
-        // Only perform raycasting and outlining if mouse is not down, or if it's down and active object is a disk.
-        if (!this.mouseDown || (this.mouseDown && this.activeObject?.disk)) {
-            const intersects = this.raycaster?.intersectObjects(this.interactiveObjects, true);
-            if(intersects?.length > 0) {
-                intersects.forEach((i) => {
-                    if(i.object.type === 'Points') {
-                        // DELETE object from intersect array
-                        intersects.splice(intersects.indexOf(i), 1);
-                    }
-                });
-                const obj = intersects[0]?.object;
-                if (obj.interactive === true) {
-                    const object = obj;
-                    this.outlinePass.selectedObjects = [object];
-                    
-                    // Translate interact text on top of object position
-                    const screenPosition = object.position.clone();
-                    screenPosition.project(this.camera);
-                    screenPosition.x = (screenPosition.x + 1)  * window.innerWidth / 2;
-                    screenPosition.y = -(screenPosition.y - 1)  * window.innerHeight / 2;
-                }
-            } else {
-                (this.outlinePass && this.outlinePass?.selectedObjects != []) && (this.outlinePass.selectedObjects = []);
-            }
-        }
-        this.composer?.render();
+  update() {
+    if (
+      this.shaderPath &&
+      this.shaderPath.uniforms &&
+      this.shaderPath.uniforms.vignette
+    ) {
+      this.shaderPath.uniforms.vignette.value =
+        this.isVignette && this.isVignette.enabled ? 0.5 : 0.0;
     }
+
+    // Only perform raycasting and outlining if mouse is not down, or if it's down and active object is a disk.
+    if (!this.mouseDown || (this.mouseDown && this.activeObject?.disk)) {
+      const intersects = this.raycaster?.intersectObjects(
+        this.interactiveObjects,
+        true
+      );
+      if (intersects?.length > 0) {
+        intersects.forEach((i) => {
+          if (i.object.type === "Points") {
+            // DELETE object from intersect array
+            intersects.splice(intersects.indexOf(i), 1);
+          }
+        });
+        const obj = intersects[0]?.object;
+        if (obj.interactive === true) {
+          const object = obj;
+          this.outlinePass.selectedObjects = [object];
+
+          // Translate interact text on top of object position
+          const screenPosition = object.position.clone();
+          screenPosition.project(this.camera);
+          screenPosition.x = ((screenPosition.x + 1) * window.innerWidth) / 2;
+          screenPosition.y = (-(screenPosition.y - 1) * window.innerHeight) / 2;
+        }
+      } else {
+        this.outlinePass &&
+          this.outlinePass?.selectedObjects != [] &&
+          (this.outlinePass.selectedObjects = []);
+      }
+    }
+    this.composer?.render();
+  }
 }

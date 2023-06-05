38af2181961b8a167144308da077c6ecc15d38d8
Amandine <quellec.amandine@gmail.com>
2023-06-05 16:18:15 +0200

    feat(letter): change animation

--- front/src/WebGL/Components/Letter/Letter.js
+++ front/src/WebGL/Components/Letter/Letter.js
@@ -1,7 +1,7 @@
 import Experience from "@/WebGL/Experience";
 import * as THREE from "three";
 import OutlineModule from "@/WebGL/Utils/OutlineModule";
-//import Cube from "../Cube/Cube";
+import { TweenMax, Power4 } from "gsap";
 
 export default class Letter {
   constructor(_position = new THREE.Vector3(0, 0, 0)) {
@@ -33,6 +33,26 @@ export default class Letter {
     });
   }
 
+  showContentDiv() {
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("letter-container");
+
+    contentDiv.innerHTML = `
+      <p style="opacity: 0;">
+          Cher ami,<br><br>
+          Je vous écris cette lettre pour partager avec vous quelques mots de Lorem Ipsum. Voici un paragraphe de texte aléatoire généré pour illustrer l'apparence d'une lettre. J'espère que vous apprécierez la simplicité et la beauté du Lorem Ipsum.<br><br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan varius aliquet. Nunc commodo eleifend eros, ac sagittis ipsum laoreet ac. Integer rhoncus tellus vel tincidunt pretium. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc feugiat nisl id scelerisque commodo. Nulla pharetra felis ut urna rutrum fringilla.<br><br>
+          Sincèrement,<br>
+          Votre ami
+      </p>
+    `;
+
+    document.body.appendChild(contentDiv);
+
+    const paragraph = contentDiv.querySelector("p");
+    TweenMax.to(paragraph, 7, { opacity: 1, ease: Power4.easeOut });
+  }
+
   setAnimation() {
     // const ropeClip = this.resource.animations[0];
     const letterClip = this.resource.animations[0];
@@ -50,6 +70,12 @@ export default class Letter {
     this.animation.actions.current = this.animation.actions.letter;
     this.animation.actions.current.setLoop(THREE.LoopOnce);
     this.animation.actions.current.timeScale = 0.0005;
+
+    this.animation.actions.current.clampWhenFinished = true;
+
+    this.animation.mixer.addEventListener("finished", () => {
+      this.showContentDiv();
+    });
   }
 
   startAnimation() {
@@ -59,12 +85,20 @@ export default class Letter {
   }
 
   update() {
+    if (!this.outlineModule.onLetter) {
+      const contentDiv = document.querySelector(".letter-container");
+      if (contentDiv && contentDiv.style.display != "none") {
+        contentDiv.style.display = "none";
+      }
+    }
+
     if (
       this.outlineModule.isLetterAnimationFinished &&
       !this.animationStarted
     ) {
       this.startAnimation();
     }
+
     this.animation?.mixer.update(this.experience.time.delta * 1);
   }
 }

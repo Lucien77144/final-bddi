5b15bc19e8f1b5a4846ded73c84bfc29cfc1873f
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2023-06-07 19:10:44 +0200

    fix(console.log): comment

--- front/src/WebGL/Scenes/Intro.js
+++ front/src/WebGL/Scenes/Intro.js
@@ -271,7 +271,6 @@ export default class Intro {
         const randomPositionIndex = Math.floor(Math.random() * this.symbolPosition.length);
         const randomPosition = this.symbolPosition[randomPositionIndex];
         this.symbolPosition.splice(randomPositionIndex, 1);
-        console.log(randomSymbolName);
         
         // Push the new Symbol to the symbols array
         this.symbols.push(new Symbol({

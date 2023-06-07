ce1dc2d6f099f26f3b4ffb8ecf829aecb31e13dc
Amandine <quellec.amandine@gmail.com>
2023-06-07 19:56:31 +0200

    feat(menu): add menu

--- front/src/script.js
+++ front/src/script.js
@@ -1,6 +1,7 @@
 import Experience from "webgl/Experience";
 // import './scripts/login';
-import './scripts/room';
-import './scripts/movement';
+import "./scripts/room";
+import "./scripts/movement";
+import "./scripts/menu";
 
 const experience = new Experience(document.querySelector("canvas#webgl"));
---
+++ front/src/scripts/menu.js
@@ -0,0 +1,55 @@
+const optionsButton = document.getElementById("options-button");
+const helpButton = document.getElementById("help-button");
+const aboutButton = document.getElementById("about-button");
+const closeButton = document.getElementsByClassName("close-button");
+
+const backButton = document.querySelector("#back-button");
+const leaveButton = document.querySelector("#leave-button");
+const clueButton = document.querySelector("#clue-button");
+
+const optionsPopup = document.querySelector(".options-popup");
+const helpPopup = document.querySelector(".help-popup");
+
+function showPopup(popup) {
+  popup.classList.add("show");
+}
+
+function hidePopup(popup) {
+  popup.classList.remove("show");
+}
+
+optionsButton.addEventListener("click", () => {
+  showPopup(optionsPopup);
+});
+
+helpButton.addEventListener("click", () => {
+  showPopup(helpPopup);
+});
+
+helpButton.addEventListener("click", () => {
+  showPopup(helpPopup);
+});
+
+backButton.addEventListener("click", () => {
+  location.reload();
+});
+
+leaveButton.addEventListener("click", () => {
+  location.reload();
+});
+
+clueButton.addEventListener("click", () => {
+  window.location.href = "about.html";
+});
+
+for (let i = 0; i < closeButton.length; i++) {
+  closeButton[i].addEventListener("click", () => {
+    hidePopup(closeButton[i].closest(".popup-overlay"));
+  });
+}
+
+window.addEventListener("click", (event) => {
+  if (event.target.classList.contains("popup-overlay")) {
+    hidePopup(event.target);
+  }
+});

c2a753b42e90c2d8894e888a03c54c350aca989c
Antoine Droyer <antoine.droyer@gmail.com>
2023-06-07 21:46:56 +0200

    fix(server) : new url

--- front/src/scripts/socket.js
+++ front/src/scripts/socket.js
@@ -6,7 +6,7 @@ import { io } from 'socket.io-client'
 // get ip address
 
 // export const socket = io('http://172.20.10.10:3000')
-export const socket = io('http://localhost:3000')
+export const socket = io('https://test-urma-server.onrender.com')
 // socket.on('connect', init)
 
 /* Imports go here */
